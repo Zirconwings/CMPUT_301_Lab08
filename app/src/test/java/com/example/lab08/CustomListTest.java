674cb7b67f1280f25f05a4c8c780059104b64254
Zirconwings <ugonnakpulonu@gmail.com>
2025-10-29 17:32:51 -0600

    Test commit

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,10 +3,19 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
-    @Test public void testHasCity(){
+    @Test
+    public void testHasCity(){
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
         assertTrue(list.hasCity(calgary));
+    }
+    @Test
+    public void testDeleteCity(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        list.deleteCity(calgary);
+        assertFalse(list.hasCity(calgary));
     }
 }
